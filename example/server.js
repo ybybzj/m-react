32e3baf951d457407b89c1c8d1fc2af880d63193
ybybzj <ybybzj@gmail.com>
2015-06-01 11:33:27 +0800

    update example

--- example/server.js
+++ example/server.js
@@ -9,5 +9,8 @@ var app = express();
 app.use(compression());
 app.use(logger('dev'));
 app.use(express.static(__dirname));
+app.get('/vendors/m-react.js', function(req,res){
+  res.sendFile($node.path.join(__dirname, '../build/m-react.js'));
+});
 app.get('/app.js', browserify('./client/app.msx',{transform: ['mithrilify']}));
 app.listen(3000);
