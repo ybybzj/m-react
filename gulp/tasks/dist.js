8d7ebe191183f25953cb1757b3d3e3ed0e40d146
ybybzj <ybybzj@gmail.com>
2015-06-01 14:52:51 +0800

    [gulp]add show gzip size

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -2,10 +2,15 @@ var path = require('path');
 module.exports = function(gulp, $, cfg) {
   var gulpCfg = cfg.gulp;
   gulp.task('dist', ['build'], function() {
+    var s = $.size({
+              title: 'm-react dist',
+              gzip: true
+            });
     return gulp.src(path.join(gulpCfg.dest.build, gulpCfg.dest.outputFile))
     .pipe($.uglify())
+    .pipe(s)
     .pipe($.rename({
       extname: '.min.js'
-    })).pipe(gulp.dest(gulpCfg.dest.dist));
+    })).pipe(gulp.dest(gulpCfg.dest.dist))
   });
 };
